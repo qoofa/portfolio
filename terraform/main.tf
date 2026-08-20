549c1e9feb07f0805b73f2400516e27b43e256f1
Minhaj <mdminhajgdr@gmail.com>
2026-08-20 12:19:31 +0000

    chore: s3 bucket policy

--- terraform/main.tf
+++ terraform/main.tf
@@ -40,3 +40,28 @@ resource "aws_s3_bucket_public_access_block" "portfoliobucket_public_access_conf
   ignore_public_acls      = false
   restrict_public_buckets = false
 }
+
+resource "aws_s3_bucket_policy" "allow_public_access" {
+  bucket = aws_s3_bucket.portfoliobucket.id
+  policy = data.aws_iam_policy_document.allow_public_access.json
+}
+
+data "aws_iam_policy_document" "allow_public_access" {
+  statement {
+    sid    = "PublicReadGetObject"
+    effect = "Allow"
+
+    principals {
+      type        = "*"
+      identifiers = ["*"]
+    }
+
+    actions = [
+      "s3:GetObject"
+    ]
+
+    resources = [
+      "arn:aws:s3:::${aws_s3_bucket.portfoliobucket.id}/*"
+    ]
+  }
+}
