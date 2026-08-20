f6fa46781686a798506684ed630b233b7a16db2c
Minhaj <mdminhajgdr@gmail.com>
2026-08-20 11:37:06 +0000

    chore: aws s3 bucket public access block

--- terraform/main.tf
+++ terraform/main.tf
@@ -31,3 +31,12 @@ output "website_endpoint" {
   description = "website endpoint of s3 web hosting"
   value       = aws_s3_bucket_website_configuration.portfoliobucket_web_config.website_endpoint
 }
+
+resource "aws_s3_bucket_public_access_block" "portfoliobucket_public_access_config" {
+  bucket = aws_s3_bucket.portfoliobucket.id
+
+  block_public_acls       = false
+  block_public_policy     = false
+  ignore_public_acls      = false
+  restrict_public_buckets = false
+}
