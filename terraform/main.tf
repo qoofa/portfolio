b2f96f6a2faf7229508a40106c1bf8b7b810a797
Minhaj <mdminhajgdr@gmail.com>
2026-08-21 18:35:20 +0000

    chore: domain name output

--- terraform/main.tf
+++ terraform/main.tf
@@ -129,3 +129,8 @@ resource "aws_cloudfront_distribution" "distribution" {
     }
   }
 }
+
+output "domain_name" {
+  value       = aws_cloudfront_distribution.distribution.domain_name
+  description = "The domain name of cloudfront"
+}
