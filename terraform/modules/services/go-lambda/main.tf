b1a0b460dec67bd28dd587df98cee948afef12a4
Minhaj <mdminhajgdr@gmail.com>
2026-08-30 10:06:11 -0700

    chore: lambda execution

--- terraform/modules/services/go-lambda/main.tf
+++ terraform/modules/services/go-lambda/main.tf
@@ -21,3 +21,66 @@ resource "aws_ecr_repository" "lambda_ecr" {
 
   force_delete = true
 }
+
+resource "aws_iam_role" "example" {
+  name = "example_lambda_execution_role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Principal = {
+          Service = "lambda.amazonaws.com"
+        }
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "lambda_basic_execution" {
+  role       = aws_iam_role.example.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
+}
+
+resource "aws_iam_policy" "dynamodb_access" {
+  name        = "example_lambda_dynamodb_policy"
+  description = "Allows Lambda full CRUD actions on all DynamoDB tables"
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Action = [
+          "dynamodb:GetItem",
+          "dynamodb:PutItem",
+          "dynamodb:UpdateItem",
+          "dynamodb:DeleteItem",
+          "dynamodb:Query",
+          "dynamodb:Scan",
+          "dynamodb:BatchGetItem",
+          "dynamodb:BatchWriteItem"
+        ]
+
+        Resource = "*"
+      }
+    ]
+  })
+}
+
+
+resource "aws_iam_role_policy_attachment" "lambda_dynamodb_attach" {
+  role       = aws_iam_role.example.name
+  policy_arn = aws_iam_policy.dynamodb_access.arn
+}
+
+resource "aws_lambda_function" "example" {
+  function_name = "example_container_function"
+  role          = aws_iam_role.example.arn
+  package_type  = "Image"
+  image_uri     = "${aws_ecr_repository.lambda_ecr.repository_url}:latest"
+
+  timeout = 30
+}
