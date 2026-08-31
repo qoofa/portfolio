4c49702424ae4260f679dea19b1f0797ce4eac3f
Minhaj <mdminhajgdr@gmail.com>
2026-08-31 01:56:20 -0700

    feat: update lambda for updated response format

--- terraform/lambda/main.go
+++ terraform/lambda/main.go
@@ -2,16 +2,36 @@ package main
 
 import (
 	"context"
+	"encoding/json"
 
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
 )
 
+type Response struct {
+	ViewCount int `json:"view_count"`
+}
+
 func handler(ctx context.Context) (events.APIGatewayProxyResponse, error) {
+	body, err := json.Marshal(Response{
+		ViewCount: 100,
+	})
+
+	if err != nil {
+		return events.APIGatewayProxyResponse{
+			StatusCode: 500,
+			Body:       `{"error":"Failed to marshall"}`,
+		}, err
+	}
+
 	response := events.APIGatewayProxyResponse{
 		StatusCode: 200,
-		Body:       "\"Hello from Lambda!\"",
+		Headers: map[string]string{
+			"Content-Type": "application/json",
+		},
+		Body: string(body),
 	}
+
 	return response, nil
 }
 
