f120fbae657f221d728927af0167f311a2a201b2
Minhaj <mdminhajgdr@gmail.com>
2026-08-31 05:03:42 -0700

    chore: implement api gateway

--- terraform/modules/services/go-lambda/output.tf
+++ terraform/modules/services/go-lambda/output.tf
@@ -2,3 +2,9 @@ output "ecr_repository_url" {
   value       = aws_ecr_repository.lambda_ecr.repository_url
   description = "ECR Repository URL"
 }
+
+output "base_url" {
+  description = "Base URL for API Gateway stage."
+
+  value = aws_apigatewayv2_stage.lambda.invoke_url
+}
