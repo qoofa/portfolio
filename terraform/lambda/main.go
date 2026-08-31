90295707d42d1e02f93796156588f689a8118307
Minhaj <mdminhajgdr@gmail.com>
2026-08-31 06:51:06 -0700

    feat: goland updating dynamodb

--- terraform/lambda/main.go
+++ terraform/lambda/main.go
@@ -6,33 +6,78 @@ import (
 
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
+	"github.com/aws/aws-sdk-go-v2/aws"
+	"github.com/aws/aws-sdk-go-v2/config"
+	"github.com/aws/aws-sdk-go-v2/service/dynamodb"
+	"github.com/aws/aws-sdk-go-v2/service/dynamodb/types"
+	"strconv"
 )
 
 type Response struct {
 	ViewCount int `json:"view_count"`
 }
 
+var dbClient *dynamodb.Client
+
+func init() {
+	cfg, err := config.LoadDefaultConfig(context.TODO())
+	if err != nil {
+		panic("Failed to load AWS SDK config: " + err.Error())
+	}
+	dbClient = dynamodb.NewFromConfig(cfg)
+}
+
 func handler(ctx context.Context) (events.APIGatewayProxyResponse, error) {
+	pageID := "home-page"
+
+	input := &dynamodb.UpdateItemInput{
+		TableName: aws.String("PageViews"),
+		Key: map[string]types.AttributeValue{
+			"id": &types.AttributeValueMemberS{Value: pageID},
+		},
+		UpdateExpression: aws.String("SET views = if_not_exists(views, :zero) + :inc"),
+		ExpressionAttributeValues: map[string]types.AttributeValue{
+			":inc":  &types.AttributeValueMemberN{Value: "1"},
+			":zero": &types.AttributeValueMemberN{Value: "0"},
+		},
+		ReturnValues: types.ReturnValueUpdatedNew,
+	}
+
+	result, err := dbClient.UpdateItem(ctx, input)
+	if err != nil {
+		return events.APIGatewayProxyResponse{
+			StatusCode: 500,
+			Headers: map[string]string{
+				"Content-Type": "application/json",
+			},
+			Body: `{"error":"Failed to update database"}`,
+		}, nil
+	}
+
+	viewsAttr := result.Attributes["views"].(*types.AttributeValueMemberN).Value
+	viewsCount, _ := strconv.Atoi(viewsAttr)
+
 	body, err := json.Marshal(Response{
-		ViewCount: 100,
+		ViewCount: viewsCount,
 	})
 
 	if err != nil {
 		return events.APIGatewayProxyResponse{
 			StatusCode: 500,
-			Body:       `{"error":"Failed to marshall"}`,
+			Headers: map[string]string{
+				"Content-Type": "application/json",
+			},
+			Body: `{"error":"Failed to marshall"}`,
 		}, err
 	}
 
-	response := events.APIGatewayProxyResponse{
+	return events.APIGatewayProxyResponse{
 		StatusCode: 200,
 		Headers: map[string]string{
 			"Content-Type": "application/json",
 		},
 		Body: string(body),
-	}
-
-	return response, nil
+	}, nil
 }
 
 func main() {
