58becd8287235e58063fbcf3a79aa3e404f95764
Minhaj <mdminhajgdr@gmail.com>
2026-08-31 23:59:48 -0700

    mend

--- terraform/modules/database/main.tf
+++ terraform/modules/database/main.tf
@@ -1,16 +1,3 @@
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 6.0"
-    }
-  }
-}
-
-provider "aws" {
-  region = "ap-south-1"
-}
-
 resource "aws_dynamodb_table" "page_views" {
   name = "PageViews"
   billing_mode = "PAY_PER_REQUEST"
