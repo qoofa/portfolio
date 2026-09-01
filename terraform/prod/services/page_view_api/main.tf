6d2d84e656395ffaaa7443d1c6db380a5550368b
Minhaj <mdminhajgdr@gmail.com>
2026-09-01 09:11:18 -0700

    feat: go lambda api

--- terraform/prod/services/page_view_api/main.tf
+++ terraform/prod/services/page_view_api/main.tf
@@ -3,3 +3,10 @@ module "dynamodb_table" {
 
   table_name = "PageViews"
 }
+
+module "go_lambda_api" {
+  source = "../../../modules/services/go-lambda"
+
+  ecr_name = "portfolio_go_api"
+  function_name = "view_count"
+}
