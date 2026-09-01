ae42e62cef98b398a450312d93a86609ba77050a
Minhaj <mdminhajgdr@gmail.com>
2026-08-31 23:59:04 -0700

    chore: migrated providers

--- terraform/modules/services/go-lambda/main.tf
+++ terraform/modules/services/go-lambda/main.tf
@@ -1,16 +1,3 @@
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 6.0"
-    }
-  }
-}
-
-provider "aws" {
-  region = "ap-south-1"
-}
-
 resource "aws_ecr_repository" "lambda_ecr" {
   name                 = "go-lambda-ecr-repository"
   image_tag_mutability = "MUTABLE"
--- terraform/modules/services/s3-cloudfront/main.tf
+++ terraform/modules/services/s3-cloudfront/main.tf
@@ -1,16 +1,3 @@
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 6.0"
-    }
-  }
-}
-
-provider "aws" {
-  region = "ap-south-1"
-}
-
 resource "aws_s3_bucket" "portfoliobucket" {
   bucket = var.bucket_name
 }
