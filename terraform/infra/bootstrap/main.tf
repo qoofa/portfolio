bc0f952a40c20c98c1e1f228f707fc1ef63e506a
Minhaj <mdminhajgdr@gmail.com>
2026-09-01 20:54:38 -0700

    feat: bootstrap module

---
+++ terraform/infra/bootstrap/main.tf
@@ -0,0 +1,24 @@
+resource "aws_s3_bucket" "tf_state" {
+  bucket = "portfolio_tf_states_1421"
+
+  lifecycle {
+    prevent_destroy = true
+  }
+}
+
+resource "aws_s3_bucket_versioning" "tf_state" {
+  bucket = aws_s3_bucket.tf_state.id
+  versioning_configuration {
+    status = "ENABLED"
+  }
+}
+
+resource "aws_s3_bucket_server_side_encryption_configuration" "tf_state" {
+  bucket = aws_s3_bucket.tf_state.id
+
+  rule {
+    apply_server_side_encryption_by_default {
+      sse_algorithm = "AES256"
+    }
+  }
+}
