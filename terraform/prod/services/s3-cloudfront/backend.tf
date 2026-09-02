6cf839b49244fc1dd05db13d91d70c7b0fcaac65
Minhaj <mdminhajgdr@gmail.com>
2026-09-02 09:51:10 -0700

    feat: update backend

--- terraform/prod/services/s3-cloudfront/backend.tf
+++ terraform/prod/services/s3-cloudfront/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "portfolio_tf_states_1421"
+    bucket = "portfolio-tf-states-1421"
     key    = "prod/services/s3-cloudfront/terraform.tfstate"
     region = "ap-south-1"
 
