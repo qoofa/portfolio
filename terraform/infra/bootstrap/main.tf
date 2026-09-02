ac19f24a2841c10fd29f6e5eb5fe483deeefa208
Minhaj <mdminhajgdr@gmail.com>
2026-09-01 21:15:06 -0700

    fix: remove service side encryption

--- terraform/infra/bootstrap/main.tf
+++ terraform/infra/bootstrap/main.tf
@@ -12,13 +12,3 @@ resource "aws_s3_bucket_versioning" "tf_state" {
     status = "ENABLED"
   }
 }
-
-resource "aws_s3_bucket_server_side_encryption_configuration" "tf_state" {
-  bucket = aws_s3_bucket.tf_state.id
-
-  rule {
-    apply_server_side_encryption_by_default {
-      sse_algorithm = "AES256"
-    }
-  }
-}
