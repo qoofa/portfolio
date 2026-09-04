75a5d655f36aaed29245a26f096d5fbf5a9499a3
Minhaj <mdminhajgdr@gmail.com>
2026-09-04 09:30:45 -0700

    feat: update s3 configs

--- terraform/modules/services/s3-cloudfront/main.tf
+++ terraform/modules/services/s3-cloudfront/main.tf
@@ -2,69 +2,21 @@ resource "aws_s3_bucket" "portfoliobucket" {
   bucket = var.bucket_name
 }
 
-resource "aws_s3_bucket_website_configuration" "portfoliobucket_web_config" {
-  bucket = aws_s3_bucket.portfoliobucket.id
-
-  index_document {
-    suffix = "index.html"
-  }
-
-  error_document {
-    key = "error.html"
-  }
-}
-
 resource "aws_s3_bucket_public_access_block" "portfoliobucket_public_access_config" {
   bucket = aws_s3_bucket.portfoliobucket.id
 
   block_public_acls       = true
-  block_public_policy     = false
-  ignore_public_acls      = false
-  restrict_public_buckets = false
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
 }
 
-resource "aws_s3_bucket_policy" "allow_public_access" {
-  bucket = aws_s3_bucket.portfoliobucket.id
-  policy = data.aws_iam_policy_document.allow_public_access.json
-}
-
-data "aws_iam_policy_document" "allow_public_access" {
-  statement {
-    sid    = "PublicReadGetObject"
-    effect = "Allow"
-
-    principals {
-      type        = "*"
-      identifiers = ["*"]
-    }
-
-    actions = [
-      "s3:GetObject"
-    ]
-
-    resources = [
-      "arn:aws:s3:::${aws_s3_bucket.portfoliobucket.id}/*"
-    ]
-  }
-}
-
-resource "aws_s3_bucket_ownership_controls" "bucket_control" {
-  bucket = aws_s3_bucket.portfoliobucket.id
-
-  rule {
-    object_ownership = "BucketOwnerPreferred"
-  }
-}
-
-resource "aws_s3_bucket_acl" "acl" {
-  depends_on = [aws_s3_bucket_ownership_controls.bucket_control]
-
-  bucket = aws_s3_bucket.portfoliobucket.id
-  acl    = "log-delivery-write"
-}
-
-resource "aws_cloudfront_origin_access_identity" "distribution_identity" {
-  comment = aws_s3_bucket.portfoliobucket.id
+resource "aws_cloudfront_origin_access_control" "oac" {
+  name                              = "${var.bucket_name}-oac"
+  description                       = "OAC for ${var.bucket_name}"
+  origin_access_control_origin_type = "s3"
+  signing_behavior                  = "always"
+  signing_protocol                  = "sigv4"
 }
 
 resource "aws_cloudfront_distribution" "distribution" {
@@ -74,12 +26,9 @@ resource "aws_cloudfront_distribution" "distribution" {
   default_root_object = "index.html"
 
   origin {
-    domain_name = aws_s3_bucket.portfoliobucket.bucket_regional_domain_name
-    origin_id   = aws_s3_bucket.portfoliobucket.id
-
-    s3_origin_config {
-      origin_access_identity = aws_cloudfront_origin_access_identity.distribution_identity.cloudfront_access_identity_path
-    }
+    domain_name              = aws_s3_bucket.portfoliobucket.bucket_regional_domain_name
+    origin_id                = aws_s3_bucket.portfoliobucket.id
+    origin_access_control_id = aws_cloudfront_origin_access_control.oac.id
   }
 
   default_cache_behavior {
@@ -100,15 +49,42 @@ resource "aws_cloudfront_distribution" "distribution" {
     cloudfront_default_certificate = true
   }
 
-  logging_config {
-    include_cookies = false
-    bucket          = aws_s3_bucket.portfoliobucket.bucket_domain_name
-    prefix          = "logs/"
-  }
-
   restrictions {
     geo_restriction {
       restriction_type = "none"
+    }
+  }
+}
+
+resource "aws_s3_bucket_policy" "allow_cloudfront_only" {
+  bucket = aws_s3_bucket.portfoliobucket.id
+  policy = data.aws_iam_policy_document.allow_cloudfront_only.json
+
+  depends_on = [aws_s3_bucket_public_access_block.portfoliobucket_public_access_config]
+}
+
+data "aws_iam_policy_document" "allow_cloudfront_only" {
+  statement {
+    sid    = "AllowCloudFrontServicePrincipalReadOnly"
+    effect = "Allow"
+
+    principals {
+      type        = "Service"
+      identifiers = ["cloudfront.amazonaws.com"]
+    }
+
+    actions = [
+      "s3:GetObject"
+    ]
+
+    resources = [
+      "${aws_s3_bucket.portfoliobucket.arn}/*"
+    ]
+
+    condition {
+      test     = "StringEquals"
+      variable = "AWS:SourceArn"
+      values   = [aws_cloudfront_distribution.distribution.arn]
     }
   }
 }
